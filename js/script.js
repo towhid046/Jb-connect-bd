05fe19da75431af7ac2c92b9465a7c4e7fb907ba
towhid046 <towhidmorol46@gmail.com>
2025-04-17 12:14:05 +0600

    make the navbar backgorund white when user scroll a bit down

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById('menu-icon')
 const menu = document.getElementById('menu-id')
+const navbar = document.getElementById('navbar-id')
 
 let isMenuOpen = false;
 
@@ -15,3 +16,18 @@ menuButton.addEventListener('click', () => {
         menuButton.innerHTML = `<i class="fas fa-bars"></i>`
     }
 })
+
+// change the background to white if user scroll more that 100px
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    if (scrollY > 100) {
+        navbar.classList.remove('bg-transparent');
+        navbar.classList.add('bg-primary', 'box-shadow');
+    } else {
+        navbar.classList.remove('bg-primary', 'box-shadow');
+        navbar.classList.add('bg-transparent');
+    }
+});
+
+
